e11a360cbcad93677a29c6053453034421a411e0
Jannis Andrija Schnitzer <jannis.schnitzer@infrontquant.com>
2021-12-15 13:51:05 +0100

    Clean up priority queue code

--- cmd/15/main.go
+++ cmd/15/main.go
@@ -59,70 +59,6 @@ func main() {
 	fmt.Println("Risk:", risk)
 }
 
-type pointPriorityQueue struct {
-	queue    []grid.Point
-	priority map[grid.Point]int
-	indexes  map[grid.Point]int
-}
-
-func newPointPriorityQueue() *pointPriorityQueue {
-	return &pointPriorityQueue{
-		priority: make(map[grid.Point]int),
-		indexes:  make(map[grid.Point]int),
-	}
-}
-
-func (pq *pointPriorityQueue) Len() int { return len(pq.queue) }
-
-func (pq *pointPriorityQueue) Less(i, j int) bool {
-	return pq.priority[pq.queue[i]] > pq.priority[pq.queue[j]]
-}
-
-func (pq *pointPriorityQueue) Swap(i, j int) {
-	pq.queue[i], pq.queue[j] = pq.queue[j], pq.queue[i]
-	pq.indexes[pq.queue[i]] = i
-	pq.indexes[pq.queue[j]] = j
-}
-
-func (pq *pointPriorityQueue) Push(x interface{}) {
-	n := len(pq.queue)
-	item := x.(grid.Point)
-	pq.indexes[item] = n
-	pq.queue = append(pq.queue, item)
-}
-
-func (pq *pointPriorityQueue) Pop() interface{} {
-	old := pq.queue
-	n := len(old)
-	item := old[n-1]
-	delete(pq.indexes, item)
-	pq.queue = old[0 : n-1]
-	return item
-}
-
-func (pq *pointPriorityQueue) pop() (grid.Point, int) {
-	p := heap.Pop(pq).(grid.Point)
-	prio := pq.priority[p]
-	delete(pq.priority, p)
-	return p, -prio
-}
-
-// decrease modifies the priority and value of an Item in the queue only if it is a decrease.
-func (pq *pointPriorityQueue) decrease(item grid.Point, priority int) bool {
-	oldPrio := pq.priority[item]
-	if oldPrio > priority {
-		return false
-	}
-	pq.update(item, priority)
-	return true
-}
-
-// update modifies the priority and value of an Item in the queue.
-func (pq *pointPriorityQueue) update(item grid.Point, priority int) {
-	pq.priority[item] = priority
-	heap.Fix(pq, pq.indexes[item])
-}
-
 func dijkstra(g integer.Grid, start, end grid.Point) []grid.Point {
 	// tentative := integer.NewGrid(g.Width(), g.Height())
 	// unvisited := make(map[grid.Point]struct{})
@@ -138,7 +74,7 @@ func dijkstra(g integer.Grid, start, end grid.Point) []grid.Point {
 				continue
 			}
 			heap.Push(tentativeQueue, p)
-			tentativeQueue.update(p, -math.MaxInt)
+			tentativeQueue.update(p, math.MaxInt)
 		}
 	}
 
@@ -166,7 +102,7 @@ func dijkstra(g integer.Grid, start, end grid.Point) []grid.Point {
 
 		for _, p := range g.Environment4(current) {
 			dist := curDist + g.MustAt(p)
-			if tentativeQueue.decrease(p, -dist) {
+			if tentativeQueue.decrease(p, dist) {
 				predecessors[p] = current
 			}
 		}
---
+++ cmd/15/pq.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"container/heap"
+
+	"github.com/Xjs/aoc2021/integer/grid"
+)
+
+type pointPriorityQueue struct {
+	queue    []grid.Point
+	priority map[grid.Point]int
+	indexes  map[grid.Point]int
+}
+
+func newPointPriorityQueue() *pointPriorityQueue {
+	return &pointPriorityQueue{
+		priority: make(map[grid.Point]int),
+		indexes:  make(map[grid.Point]int),
+	}
+}
+
+// Len implements sort.Interface
+func (pq *pointPriorityQueue) Len() int { return len(pq.queue) }
+
+// Less implements sort.Interface (with a greater-than, to invert sort order to make it a priority queue)
+func (pq *pointPriorityQueue) Less(i, j int) bool {
+	return pq.priority[pq.queue[i]] > pq.priority[pq.queue[j]]
+}
+
+// Swap implements sort.Interface
+func (pq *pointPriorityQueue) Swap(i, j int) {
+	pq.queue[i], pq.queue[j] = pq.queue[j], pq.queue[i]
+	pq.indexes[pq.queue[i]] = i
+	pq.indexes[pq.queue[j]] = j
+}
+
+// Push implements heap.Interface
+func (pq *pointPriorityQueue) Push(x interface{}) {
+	n := len(pq.queue)
+	item := x.(grid.Point)
+	pq.indexes[item] = n
+	pq.queue = append(pq.queue, item)
+}
+
+// Pop implements heap.Interface
+func (pq *pointPriorityQueue) Pop() interface{} {
+	old := pq.queue
+	n := len(old)
+	item := old[n-1]
+	delete(pq.indexes, item)
+	pq.queue = old[0 : n-1]
+	return item
+}
+
+// pop returns the point with lowest priority and its priority.
+func (pq *pointPriorityQueue) pop() (grid.Point, int) {
+	p := heap.Pop(pq).(grid.Point)
+	prio := pq.getPriority(p)
+	delete(pq.priority, p)
+	return p, prio
+}
+
+func (pq *pointPriorityQueue) getPriority(item grid.Point) int {
+	return -pq.priority[item]
+}
+
+// decrease modifies the priority and value of a point in the queue only if the priority is lower than the current priority.
+// It returns true if a modification was made.
+func (pq *pointPriorityQueue) decrease(item grid.Point, priority int) bool {
+	oldPrio := pq.getPriority(item)
+	if oldPrio < priority {
+		return false
+	}
+	pq.update(item, priority)
+	return true
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *pointPriorityQueue) update(item grid.Point, priority int) {
+	priority = -priority
+	pq.priority[item] = priority
+	heap.Fix(pq, pq.indexes[item])
+}
